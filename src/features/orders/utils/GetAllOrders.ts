afe1bfeaf3ecd1a30e85568cc1d0bc1e993987f8
daniel8298 <daniel8298@gmail.com>
2023-11-23 10:01:22 +0200

    port

--- src/features/orders/utils/GetAllOrders.ts
+++ src/features/orders/utils/GetAllOrders.ts
@@ -10,7 +10,7 @@ const GetAllOrders = () => {
 
   useEffect(() => {
     axios
-      .get("http://localhost:3333/api/orders")
+      .get("http://localhost:3000/api/orders")
       .then((res) => {
         dispatch(setOrders(res.data));
         console.log("Success");
