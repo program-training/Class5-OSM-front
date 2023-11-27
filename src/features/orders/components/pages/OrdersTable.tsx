1e2c8e73404283b29eb6f6ac4e2334424eb83b73
daniel8298 <daniel8298@gmail.com>
2023-11-27 14:22:31 +0200

    update

--- src/features/orders/components/pages/OrdersTable.tsx
+++ src/features/orders/components/pages/OrdersTable.tsx
@@ -21,7 +21,7 @@ const OrdersTable = () => {
   const { page, handleChangePage, handleChangeRowsPerPage, rowsPerPage } =
     usePages();
   const { searchTerm } = useTerm();
-  const { changeStatus, handleCancel, handleReceive } = useOrder(orders);
+  const { changeStatus } = useOrder(orders);
   const filteredOrders = filterArrayOfObjects(
     orders,
     "_id" || "shippingDetails.userId",
@@ -67,41 +67,3 @@ const OrdersTable = () => {
 };
 
 export default OrdersTable;
-
-// export default function PaginationControlled() {
-//   const [page, setPage] = React.useState(1);
-//   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-//     setPage(value);
-//   };
-
-//   return (
-//     <Stack spacing={2}>
-//       <Typography>Page: {page}</Typography>
-//       <Pagination count={10} page={page} onChange={handleChange} />
-//     </Stack>
-//   );
-// }
-/////////////////////////////////////////////
-
-// import * as React from 'react';
-// import Pagination from '@mui/material/Pagination';
-// import PaginationItem from '@mui/material/PaginationItem';
-// import Stack from '@mui/material/Stack';
-// import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-// import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-
-// export default function CustomIcons() {
-//   return (
-//     <Stack spacing={2}>
-//       <Pagination
-//         count={10}
-//         renderItem={(item) => (
-//           <PaginationItem
-//             slots={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-//             {...item}
-//           />
-//         )}
-//       />
-//     </Stack>
-//   );
-// }
