3723dbe75846c45ea3af5bda4d22edca24cd332e
daniel8298 <daniel8298@gmail.com>
2023-11-30 07:10:23 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,27 @@ import { CssBaseline } from "@mui/material";
 import "./App.css";
 import Header from "./features/layout/Header/Header";
 import RouterDOM from "./features/router/RouterDOM";
+// import { useEffect, useState } from "react";
+// import Spinner from "./Spinner";
 
 const App = () => {
   const themeMode = useAppSelector((store) => store.themeMode.themeMode);
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   setLoading(false);
+  // }, []);
+
+  // if (loading) {
+  //   return (
+  //     <>
+  //       <ThemeProvider theme={themeMode ? themeLight : themeDark}>
+  //         <CssBaseline />
+  //         <Spinner />;
+  //       </ThemeProvider>
+  //     </>
+  //   );
+  // }
 
   return (
     <>
